2413492a193fc20e9002a9c948fdfa4cffabc775
Berlet Maxim <berletmaximq@gmail.com>
2026-03-04 17:48:16 +0300

    started work

---
+++ src/parser.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple
+
+class Token:
+    pass
+
+class Node:
+    pass
+
+class Parser:
+    def __init__(self, tokens: List[Token]):
+        pass
+    
+    def parse(self) -> Node:
+        pass
